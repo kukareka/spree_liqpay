8cfa072b4d6697da517dc001a1ec17ad5c224149
Oleg Kukareka <oleg@kukareka.com>
2015-05-12 15:54:53 +0300

    Move tests to spec dirrectory

--- lib/spree_liqpay/engine.rb
+++ lib/spree_liqpay/engine.rb
@@ -8,7 +8,10 @@ module SpreeLiqpay
 
     # use rspec for tests
     config.generators do |g|
-      g.test_framework :rspec
+      g.test_framework :rspec, fixture: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.assets false
+      g.helper false
     end
 
     def self.activate
